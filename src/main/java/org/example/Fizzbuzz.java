6321adb7cfce29c8d5ddbcccd5d9d4f86b9f47e0
juan ignacio <nho13@hotmail.com>
2024-10-31 14:52:38 +0100

    feat: saving local

---
+++ src/main/java/org/example/Fizzbuzz.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Fizzbuzz {
+    public String checkNumber(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        return null;
+
+    }
+
+    public class Buzzfizz {
+        public String checknumero(int number) {
+            if (number % 5 == 0) {
+                return "Buzz";
+            }
+            return null;
+        }
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,30 @@
+import org.example.Fizzbuzz;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    @DisplayName("test si el numero es divisible por 3")
+    void test_whenthenumberisdivisiblebythree_thenReturnFizz() {
+
+        //given dado
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+        //when cuando
+        String result = fizzbuzz.checkNumber(9);
+        //then pues
+        assertEquals("Fizz", result);
+
+
+    }
+
+    @Test
+    @DisplayName("test if the number is divisible by 5")
+    void test_ifnumberisdivisibleby5_thenreturnBuzz() {
+
+        Fizzbuzz buzzFizz = new Fizzbuzz();
+        String result= buzzFizz.checkNumber(10);
+
+    }
+}
